9dbd2af18c6362bdcf2b018a7831c362d1f1f4d8
root <ryanjmack21@gmail.com>
2018-09-28 21:11:13 -0400

    Add top level styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import './stylesheets/styles.css';
+
+
+// add classes to the body and html element for basic styling
+document.body.classList.add('body');
+document.querySelector('html').classList.add('html');
+
 
 ReactDOM.render(
   <App />,
